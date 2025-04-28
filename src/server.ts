a24ff5cda6a2132e291847f8c1067d1ea8faaa41
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-04-28 11:17:30 +0600

    feat: add ESLint configuration and scripts for linting
    - Added ESLint scripts to package.json for linting and fixing issues.
    - Included ESLint and TypeScript ESLint dependencies in devDependencies.
    - Created a new ESLint configuration file (eslint.config.mjs) with recommended settings for JavaScript and TypeScript.
    - Updated server.ts to improve code formatting and structure.

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,17 @@ import config from "./app/config";
 const mongoose = require('mongoose');
 
 async function main() {
-  try{
+  try {
     await mongoose.connect(config.database_url as string);
-  
+
     app.listen(config, () => {
       console.log(`Example app listening on port ${config.port}`)
-  })
+    })
 
-  }catch (error) {
+  } catch (error) {
     console.log(error);
   }
-  }
+}
+
+main()
 
