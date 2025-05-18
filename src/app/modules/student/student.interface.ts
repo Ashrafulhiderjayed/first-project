249e04966a51d3b62e7de62e50cbca47eeefa82d
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-05-18 12:21:06 +0600

    feat: format student interface for improved readability and consistency

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,42 +1,48 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model, connect, Model } from 'mongoose';
 
 //these are the sub-types of studentSchema
 export type UserName = {
-    firstName: string;
-    middleName?: string;
-    lastName: string;
-}
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
 
 export type Guardian = {
-    fatherName: string;
-    fatherOccupation: string;
-    fatherContactNo: string;
-    motherName: string;
-    motherOccupation: string;   
-    motherContactNo: string;
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female' | 'other'; //union type
+  dateOfBirth?: string; // YYYY-MM-DD format
+  email: string;
+  contactNumber: string;
+  emergencyContact: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'O+' | 'O-' | 'AB+' | 'AB-'; // union type
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian?: LocalGuardian;
+  profileImg?: string; // URL to the image
+  isActive: 'active' | 'blocked'; // union type
 };
 
 
-export type LocalGuardian = {
-    name: string;
-    occupation: string;
-    contactNo: string;
-    address: string;
+export type StudentMethods = {
+    isUserExist(id: string): Promise<Student>
 }
 
-export type Student = {
-    id: string;
-    name: UserName,
-    gender: 'male' | 'female' | 'other'; //union type 
-    dateOfBirth?: string; // YYYY-MM-DD format
-    email: string;
-    contactNumber: string;
-    emergencyContact: string;
-    bloodGroup?: "A+" | "A-" | "B+" | "B-" | "O+" | "O-" | "AB+" | "AB-"; // union type
-    presentAddress: string;
-    permanentAddress: string;
-    guardian: Guardian;
-    localGuardian?: LocalGuardian;
-    profileImg?: string; // URL to the image
-    isActive: 'active' | 'blocked'; // union type
-}
+type StudentModel = Model<Student, Record<string, never>, StudentMethods>;
