3ee18ba06ac9f9ab7afc34b7f8574023471b5aa5
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-06-24 12:51:17 +0600

    fix: update user schema and validation to include default values for status and isDeleted

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,10 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string().min(1).max(50),
   password: z
-    .string()
-    .max(20, { message: 'Password must be less than 20 characters' }),
-  needsPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(['admin', 'student', 'faculty']),
+    .string({invalid_type_error: 'Password must be a string'})
+    .max(20, { message: 'Password must be less than 20 characters' })
+    .optional(),
   status: z.enum(['in-progress', 'blocked']).default('in-progress'),
   isDeleted: z.boolean().optional().default(false), 
 });
