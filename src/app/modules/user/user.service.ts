9f9f1e6c35b0094c15aac769fb4cc2719d929cee
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-07-12 12:09:26 +0600

    refactor: clean up import statements and improve formatting in user service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,12 @@
-import config from "../../config";
-import { TStudent } from "../student/student.interface";
-import { NewUser, TUser } from "./user.interface";
-import { User } from "./user.model";
+import config from '../../config';
+import { TStudent } from '../student/student.interface';
+import { Student } from '../student/student.model';
+import { TUser } from './user.interface';
+import { User } from './user.model';
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
- // create a user object
-  const userData: Partial<TUser> = {}
+  // create a user object
+  const userData: Partial<TUser> = {};
 
   userData.password = password || config.default_password; // if password is not provided, use default password
 
@@ -14,24 +15,18 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   // set manually generated id
   userData.id = '2030100001';
 
-  // create a user 
+  // create a user
   const newUser = await User.create(userData);
 
-  // create a student 
-  if( Object.keys(newUser).length === 0 ){
+  // create a student
+  if (Object.keys(newUser).length === 0) {
     // set id , _id as user
     studentData.id = newUser.id;
-    studentData.user = newUser._id; // set user id as _id of user
+    studentData.user = newUser._id; // reference _id of User model
+
+    const newStudent = await Student.create(studentData); // create student in DB
+    return newStudent;
   }
-
-  // const student = new Student(studentData); //creating instance of model
-
-  // if(await student.isUserExist(studentData.id)){
-  //   throw new Error('User already exists!')
-  // }
-
-  // const result = await student.save(); //built in instance method of mongoose
-  return newUser;
 };
 
 export const UserServices = {
